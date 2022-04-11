cccf970912a6843e65692a754483f9115aba8ffc
bnjmn21 <benma2321@gmail.com>
2022-04-11 12:02:52 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,37 @@
+use regex::{Regex, Matches};
+use clap::Parser;
+
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    file: String,
+}
+
 fn main() {
-    let sus = regexTest::find(String::from("lorem ipsum dolor sit amet."), String::from("lor"), Vec::new());
-    println!("{:?}", sus);
+    let args = Cli::parse();
+    let mut code_line = 0u32;
+
+    let mut isRunning = true;
+
+    while isRunning {
+        let mut file = File::open(&args.file).unwrap();
+        let mut filestr = String::new();
+        file.read_to_string(&mut filestr).unwrap();
+
+        let mut lines : Vec<&str>= filestr.lines().collect();
+        
+        if lines.get(code_line as usize) == None {code_line = 0}
+        else if lines[code_line as usize] == "SUCESS" {return}
+        else if lines[code_line as usize].starts_with('!') {
+            let checkx = Regex::new().unwrap();
+
+            fs::write(&args.file, );
+        }
+
+    }
 }
--- src/regexTest.rs
+++ src/regexTest.rs
@@ -4,36 +4,5 @@ pub struct FindResult {
     end: u32,
 }
 
-pub fn find(input: String, rgx: String, flags: Vec<char>) -> Vec<FindResult> {
-    let chars:Vec<char>= input.chars().collect();
-    let rgx_char:Vec<char> = rgx.chars().collect();
-    let mut res:Vec<FindResult> = Vec::new();
-    if rgx_char.is_empty() {
-        res =  vec![FindResult{start: 0, end: (chars.len() as u32)-1}];
-    }
-    let i = 0u32;
-    for c in &chars {
-        if c == &rgx_char[i as usize] {
-            let mut is_checking = true;
-            let mut check_i = i as usize;
-            while is_checking {
-                println!("{:?} {:?}",chars.get(check_i),rgx_char.get((check_i as u32 -i) as usize));
-                if chars.get(check_i) == None {
-                    return res;
-                } else if rgx_char.get((check_i as u32 -i) as usize) == None {
-                    res.push(FindResult{start: i, end: check_i as u32});
-                    if flags.contains(&'s') {
-                        return res;
-                    } else {
-                        is_checking = false;
-                    }
-                } else if &chars[check_i] == &rgx_char[(check_i as u32 -i) as usize] {
-                    check_i += 1;
-                } else {
-                    is_checking = false;
-                }
-            }
-        }
-    }
-    return res;
+fn check_at(find_str: String, regex: String, start_pos: u32) {
 }
