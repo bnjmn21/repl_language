3b634c2c892e7a64535b8722b2ffb5aeeb0e6993
bnjmn21 <benma2321@gmail.com>
2022-04-11 13:24:49 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use clap::Parser;
 
 mod regex;
 
+/*
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::fs;
+*/
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -16,7 +18,8 @@ struct Cli {
 fn main() {
     //let args = Cli::parse();
     let teststr = String::from("sussy baka lol");
-    let findstr = String::from("baka ");
-    let testres = regex::check_at(teststr, findstr, 6);
+    let findstr = String::from("a");
+    let flags = String::from("");
+    let testres = regex::find(teststr, findstr, flags);
     println!("{:?}", testres);
 }
--- src/regex.rs
+++ src/regex.rs
@@ -10,21 +10,47 @@ pub enum CheckResult {
     Success{end: u32},
 }
 
+#[derive(Debug)]
+pub enum FindResult {
+    Fail,
+    Success{pos: FindPos}
+}
+
 pub fn check_at(find_str: String, regex: String, start_pos: u32) -> CheckResult{
     let strlist: Vec<char> = find_str.split_at(start_pos as usize).1.chars().collect();
     let rgxlist: Vec<char> = regex.chars().collect();
 
-    let mut i = start_pos as usize;
+    let mut i = 0;
 
-    for c in strlist.iter() {
+    loop{
         if rgxlist.get(i) == None {return CheckResult::Success { end:i as u32 + start_pos}}
-        match c {
+        if strlist.get(i) == None {return CheckResult::Fail}
+        match strlist.get(i) {
+            None => {
+                if rgxlist.get(i) == None {return CheckResult::Success { end:i as u32 + start_pos}}
+                else {return CheckResult::Fail}
+            }
             _ => {
-                if c == &rgxlist[i] {} else {return CheckResult::Fail}
+                if strlist[i] == rgxlist[i] {} else {return CheckResult::Fail}
             }
         }
-        println!("{:?} {:?} {:?}", i, strlist[i], rgxlist[i]);
         i += 1;
     }
-    return CheckResult::Fail
+}
+
+pub fn find(find_str: String, regex: String, flagstr: String) -> FindResult {
+    let flags: Vec<char> = flagstr.chars().collect();
+
+    let mut i = 0;
+    let mut res = FindResult::Fail;
+
+    for c in find_str.chars() {
+        let checkres = check_at(find_str.clone(), regex.clone(), i);
+        match checkres {
+            CheckResult::Fail => {res = res},
+            CheckResult::Success{ end: e } => res = FindResult::Success { pos: FindPos{start:i, end:e-1}}
+        };
+        i += 1;
+    }
+    return res;
 }
